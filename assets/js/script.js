eaeb7272efb4e0ceeb823e158f4fe7665a90aa34
Emma Sadler <summonerskies@gmail.com>
2024-10-18 12:09:12 +0000

    SCRIPT: set up structure for game functionality

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+// run game
+function runGame() {
+
+}
+
+// check answer
+function checkAnswer() {
+
+}
+
+// calculateCorrectAnswer
+function calculateCorrectAnswer() {
+
+}
+
+// incrementScore
+function incrementScore() {
+
+}
+
+// incrementWrongAnswer
+function incrementWrongAnswer() {
+
+}
+
+// displayAdditionQuestion
+function displayAdditionQuestion() {
+
+}
+// displaySubtractQuestion
+function displaySubtractQuestion() {
+
+}
+// displayMulyiplyQuestion
+function displayMulyiplyQuestion () {
+    
+}
+// displayDivisionQuestion
